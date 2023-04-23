e5117a68acbbeb861f6ee2e92b81b6f60c671e3b
nguyen ngoc khanh <84921391+hellokhanhne@users.noreply.github.com>
2023-04-23 16:41:00 +0700

    change api url

--- api/axios.ts
+++ api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://192.168.1.9:8083';
+export const BASE_URL = 'http://178.128.24.121';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
